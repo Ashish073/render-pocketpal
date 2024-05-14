9a032cb80aabf85c60bc3d78c797c20008a1c01c
Ashish Karapurker <38071797+Ashish073@users.noreply.github.com>
2024-05-14 12:25:05 +0530

    Added cors origins

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ const authRoutes = require('./routes/authRoute');
 const categoriesRoute = require('./routes/categoriesRoute');
 const expenseRoute = require('./routes/expenseRoute');
 
+const allowedOrigins = ['http://localhost', 'https://pocketpal-w5fr.onrender.com'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+
+
 app.use(express.static(path.join(__dirname, './client/build')));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -23,7 +37,7 @@ app.use("/api/expenses", expenseRoute);
 const port = process.env.PORT || 5000;
 
 
-app.get('*', () => (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
